08d35fa32a8c06cc9aeb1379b04153e519789a18
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-09-20 21:47:09 -0300

    feat(mobile/screens): add home screen component

---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: 110,
+    marginBottom: 58,
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 54,
+  },
+});
