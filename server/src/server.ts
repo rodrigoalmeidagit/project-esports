502f12d7b358b05e5ab10418ad8df134906ce710
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-09-21 23:12:37 -0300

    refactor(server/routs): add get and posts games and ads arguments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import {PrismaClient} from '@prisma/client';
+import cors from 'cors';
 import express from 'express';
+import {convertHoursStringToMinutes} from './utils/convert-hours-string-to-minutes';
+import {convertMinutesToHourString} from './utils/convert-minutes-to-hour-string';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const prisma = new PrismaClient({
   log: ['query'],
 });
@@ -61,22 +67,48 @@ app.get('/games/:id/ads', async (request, response) => {
       return {
         ...ad,
         weekDays: ad.weekDays.split(','),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
       };
     })
   );
 });
 
-app.get('/ads', (request, response) => {
-  return response.json([]);
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return response.json({
+    discord: ad.discord,
+  });
 });
 
-app.get('/games/:id/discord', (request, response) => {
-  // const adsId = request.params.id
-  return response.status(201).json([]);
-});
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const body: any = request.body;
 
-app.post('/ads', (request, response) => {
-  return response.status(201).json([]);
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHoursStringToMinutes(body.hourStart),
+      hourEnd: convertHoursStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(ad);
 });
 
 app.listen(3333);
