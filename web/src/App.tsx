a0a1f685122fda43ff393c5a15fba69064eb0ffe
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-09-22 17:13:33 -0300

    feat(modal): add radix modal basic structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 // import logoImg from './assets/logo-esports.svg';
+import * as Dialog from '@radix-ui/react-dialog';
 import { useEffect, useState } from 'react';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { GameBanner } from './components/GameBanner';
@@ -46,7 +47,19 @@ function App() {
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+            <Dialog.Title className='text-3xl font-black'>
+              Publique um anúncio
+            </Dialog.Title>
+            <Dialog.Content>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
 
     </div>
   )
