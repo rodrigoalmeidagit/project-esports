6b1262186e579dc0bb9576bac82d631824d868f5
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-09-26 15:04:22 -0300

    refactor(component): add create ad modal component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,8 @@
-// import logoImg from './assets/logo-esports.svg';
-// import { GameController } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { useEffect, useState } from 'react';
 
 import { CreateAdBanner } from './components/CreateAdBanner';
-import { Form } from './components/Form/Form';
-
+import { CreateAdModal } from './components/CreateAdModal';
 import { GameBanner } from './components/GameBanner';
 
 import './styles/main.css';
@@ -54,15 +51,7 @@ function App() {
 
       <Dialog.Root>
         <CreateAdBanner />
-        <Dialog.Portal>
-          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
-          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
-            <Dialog.Title className='text-3xl font-black'>
-              Publique um anúncio
-            </Dialog.Title>
-            <Form />
-          </Dialog.Content>
-        </Dialog.Portal>
+        <CreateAdModal />
       </Dialog.Root>
 
     </div>
